af2a107aafbb581b01cfad4e5e6d1c10f5b6a6bf
Hubi <41549340+hubg84@users.noreply.github.com>
2025-11-10 10:02:25 +0100

    Update Arukereso.py -- haedless mod added

--- .github/workflows/Arukereso.py
+++ .github/workflows/Arukereso.py
@@ -2,8 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # nincs ablak
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=chrome_options)
+
 driver.get("https://arukereso.hu")
 driver.maximize_window()
 wait = WebDriverWait(driver, 10)
